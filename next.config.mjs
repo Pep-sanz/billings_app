5a03c2b67666188eebf2101eac2bf9bf19f32fa9
PepepSaepulR <pepepsr4@gmail.com>
2025-01-06 03:15:48 +0700

    fix build cookie

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+    COOKIE_NAME: process.env.COOKIE_NAME,
+    // MIDTRANS_CLIENT_KEY: process.env.MIDTRANS_CLIENT_KEY,
+  },
+};
 
 export default nextConfig;
